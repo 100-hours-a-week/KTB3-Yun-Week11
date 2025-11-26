1aef4f7f73f20301f1981b7e0fc26866fda4ad4a
Yun <mosy2266@naver.com>
2025-11-26 16:04:40 +0900

    feat: common.js apiFetch() credentials 추가

--- iLGum/src/api/common.js
+++ iLGum/src/api/common.js
@@ -2,5 +2,6 @@ export const API_BASE = 'http://127.0.0.1:8080'
 export const apiFetch = (path, options={}) => 
     fetch(`${API_BASE}${path}`, {
         ...options,
+        credentials: options.credentials ?? 'include',
         headers: { 'Content-Type': 'application/json', ...(options.headers || {}) },
     })
--- iLGum/src/pages/Login.jsx
+++ iLGum/src/pages/Login.jsx
@@ -28,7 +28,6 @@ export default function LoginPage () {
 
         const res = await apiFetch('/members/session', {
             method: 'POST',
-            credentials: 'include',
             body: JSON.stringify({ email, password }),
         })
 
